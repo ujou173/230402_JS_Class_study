851e46e4a2d9c9e092f9ab707f6234a458014f08
Rosumin <ujou173@gmail.com>
2023-04-02 15:23:08 +0900

    class 객체 만들어보기

---
+++ class.js
@@ -0,0 +1,12 @@
+// 클래스 선언
+class student {
+  constructor(name, age, gender) {
+    this.name = name;
+    this.age = age;
+    this.gender = gender;
+  }
+}
+
+// 클래스를 이용한 객체 만들기
+let a = new student('rosumin', '28', 'male');
+console.log(a);
